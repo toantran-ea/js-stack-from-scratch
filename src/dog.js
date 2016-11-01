487cbbc8ef0afea8b1550e7fcc93049ca062a914
toan.tran <me@toan.mobi>
2016-11-01 12:32:19 +0700

    https://github.com/verekia/js-stack-from-scratch/tree/master/tutorial/5-es6-modules-syntax
    [es6-class and modules]

---
+++ src/dog.js
@@ -0,0 +1,9 @@
+export default class {
+	constructor(name) {
+		this.name = name;
+	}
+
+	bark() {
+		return `Woof woof, I am ${this.name}`;
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-const str = 'ES6';
-console.log(`Hello ${str}`);
+import Cat from './dog';
+
+const toby = new Cat('Toby');
+
+console.log(toby.bark());
+
